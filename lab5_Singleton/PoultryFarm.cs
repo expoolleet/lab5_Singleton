ec84b970e369ecc969ce3ee5751c9c4cec399386
user <user@228-04>
2022-10-25 09:51:00 +0300

    Добавьте файлы проекта.

---
+++ lab5_Singleton/PoultryFarm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_Singleton
+{
+    class PoultryFarm
+    {
+        static object locker = new();
+        private string[] ducks;
+        private static PoultryFarm farm;
+        private PoultryFarm(string[] ducksArray)
+        {
+            ducks = new string[ducksArray.Length];
+            for (int i = 0; i < ducksArray.Length; i++)
+            {
+                ducks[i] = ducksArray[i];
+            }
+        }
+  
+        public static PoultryFarm Get(string[] ducks)
+        {
+            if (farm == null)
+            {
+                lock (locker)
+                {
+                    farm = new PoultryFarm(ducks);
+                }
+            }
+            return farm;
+
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Утки: ");
+            for (int i = 0; i < ducks.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {ducks[i]}");
+            }
+        }
+    }
+}
---
+++ lab5_Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+
+namespace lab5_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ducks = { "Домашняя утка", "Дикая утка", "Железная утка" };
+
+            for (int i = 1; i < 6; i++)
+            {
+                Thread thread = new Thread(Print);
+            }
+            PoultryFarm farm = PoultryFarm.Get(ducks);
+
+
+            farm.Info();
+
+            Console.ReadKey();
+        }
+
+        private static void Print(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print() { Thread.Sleep(100); }
+    }
+}
