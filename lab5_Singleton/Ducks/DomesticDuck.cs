0774917f11ae3cd14f84681c50be6075d3758420
Г <gosha.shcherbak@mail.ru>
2022-10-27 09:49:54 +0300

    Test

---
+++ lab5_Singleton/Ducks/DomesticDuck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_Singleton.Ducks
+{
+    class DomesticDuck : Duck
+    {
+        public DomesticDuck() : base("Домашнаяя утка") { }
+    }
+}
